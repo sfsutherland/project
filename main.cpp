a1df1892f186524270fe04a3b21d949ad727965b
Stephen Sutherland <11708004+sfsutherland@users.noreply.github.com>
2023-06-17 21:57:44 -0600

    add trivial logging

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //#include "AccountParsers/AccountParser.h"
 
 #include <iostream>
+#include <boost/log/trivial.hpp>
 #include "PROJECT_config.h"
 
 int main(int argc, char**argv)
@@ -18,5 +19,14 @@ int main(int argc, char**argv)
     //      }
     // ReportCruncher.GenerateFinalReport(outputDir);
     std::cout << "This is project, version " << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR << "\n";
+    std::cout << "Source dir: " << SRC_DIR << "\nBinary dir: " << BIN_DIR << "\n";
+
+    BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+    BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+    BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+    BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
+    BOOST_LOG_TRIVIAL(error) << "An error severity message";
+    BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+    
     return 0;
 }
